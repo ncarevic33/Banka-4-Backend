f4b4ffb6258861cb3c922374f389ce06b08a67f9
JelenaMijatovic <jmijatovic11421rn@raf.rs>
2024-03-10 19:07:52 +0100

    implementiranje modela za racun

--- user-service/src/main/java/rs/edu/raf/racun/model/TekuciRacun.java
+++ user-service/src/main/java/rs/edu/raf/racun/model/TekuciRacun.java
@@ -1,4 +1,62 @@
 package rs.edu.raf.racun.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class TekuciRacun {
+
+    @Id
+    @Column(unique = true)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    private Long id;
+
+    @Column(unique = true)
+    @NotBlank
+    private String brojRacuna;
+
+    @NotNull
+    private Long vlasnik; //Korisnik id
+
+    @NotNull
+    private BigDecimal stanje;
+
+    @NotNull
+    private BigDecimal raspolozivoStanje;
+
+    @NotNull
+    private Long zaposleni; //Radnik id
+
+    @NotNull
+    private Long datumKreiranja;
+
+    @NotNull
+    private Long datumIsteka;
+
+    @NotBlank
+    private String currency;
+
+    @NotNull
+    private Boolean aktivan;
+
+    @NotBlank
+    private String vrstaRacuna; //Poslovni, Lični, Štedni, Penzionerski, Devizni
+
+    @NotNull
+    private BigDecimal kamatnaStopa; //procenat
+
+    @NotNull
+    private BigDecimal odrzavanjeRacuna;
 }
