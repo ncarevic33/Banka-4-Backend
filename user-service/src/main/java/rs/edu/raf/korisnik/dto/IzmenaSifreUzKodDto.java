7b1af4835a527015caa92311fa9f2cb3c0b6d7ef
mmihailovic <marko.mihailovic67@yahoo.com>
2024-03-11 19:51:03 +0100

    validation

--- user-service/src/main/java/rs/edu/raf/korisnik/dto/IzmenaSifreUzKodDto.java
+++ user-service/src/main/java/rs/edu/raf/korisnik/dto/IzmenaSifreUzKodDto.java
@@ -1,5 +1,6 @@
 package rs.edu.raf.korisnik.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class IzmenaSifreUzKodDto {
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Email adresa mora biti validna!")
     private String email;
     private String sifra;
     private String kod;
