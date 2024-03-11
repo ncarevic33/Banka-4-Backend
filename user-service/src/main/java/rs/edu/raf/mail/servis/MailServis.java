abd6cc55aacce31a45e07c8f651d44b68791b031
mrakicc <mrakic8121rn@raf.rs>
2024-03-11 20:01:39 +0100

    Slanje email-a pri registrovanju i promeni lozinke

--- user-service/src/main/java/rs/edu/raf/mail/servis/MailServis.java
+++ user-service/src/main/java/rs/edu/raf/mail/servis/MailServis.java
@@ -1,8 +1,9 @@
 package rs.edu.raf.mail.servis;
 
-import rs.edu.raf.korisnik.model.Korisnik;
+import rs.edu.raf.korisnik.dto.IzmenaKorisnikaDTO;
+import rs.edu.raf.korisnik.dto.NoviKorisnikDTO;
 
 public interface MailServis {
-    public boolean posaljiMailZaRegistraciju(Korisnik korisnik,String kod);
-    public boolean posaljiMailZaPromenuLozinke(Korisnik korisnik,String kod);
+    public boolean posaljiMailZaRegistraciju(NoviKorisnikDTO korisnik, String kod);
+    public boolean posaljiMailZaPromenuLozinke(IzmenaKorisnikaDTO korisnik, String kod);
 }
