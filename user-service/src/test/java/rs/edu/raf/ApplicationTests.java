7b564d2f9a0a58c978a6288d0b8c0dbef0c2657e
Petar_Stamenic_RN_77_21 <92359907+PetarStamenic@users.noreply.github.com>
2024-03-14 18:17:47 +0100

    Update ApplicationTests.java

--- user-service/src/test/java/rs/edu/raf/ApplicationTests.java
+++ user-service/src/test/java/rs/edu/raf/ApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@Disabled
 public class ApplicationTests {
 
     @Test
