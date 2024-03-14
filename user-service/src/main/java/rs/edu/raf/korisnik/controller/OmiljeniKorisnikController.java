c87bc980e8e0a8233426e46a84313d9bb4e83229
Petar_Stamenic_RN_77_21 <92359907+PetarStamenic@users.noreply.github.com>
2024-03-14 18:10:00 +0100

    Update OmiljeniKorisnikController.java

--- user-service/src/main/java/rs/edu/raf/korisnik/controller/OmiljeniKorisnikController.java
+++ user-service/src/main/java/rs/edu/raf/korisnik/controller/OmiljeniKorisnikController.java
@@ -23,19 +23,19 @@ public class OmiljeniKorisnikController {
 
     @ApiOperation(value = "Dodaj Omiljenog Korisnika")
     @PostMapping("")
-    public ResponseEntity<OmiljeniKorisnikDTO> dodajOmiljenogKorisnika(@RequestHeader("Authorization") String authorization, @RequestBody OmiljeniKorisnikDTO omiljeniKorisnikDTO){
+    public ResponseEntity<OmiljeniKorisnikDTO> dodajOmiljenogKorisnika(@RequestBody OmiljeniKorisnikDTO omiljeniKorisnikDTO){
         return new ResponseEntity<>(omiljeniKorisnikServis.add(omiljeniKorisnikDTO), HttpStatus.OK);
     }
 
     @ApiOperation(value = "Dohvati Omiljenog Korisnika po id")
     @GetMapping("/{id}")
-    public ResponseEntity<OmiljeniKorisnikDTO> dohvatiOmiljenogKorisnika(@RequestHeader("Authorization") String authorization, @PathVariable("id") Long id){
+    public ResponseEntity<OmiljeniKorisnikDTO> dohvatiOmiljenogKorisnika(@PathVariable("id") Long id){
         return new ResponseEntity<>(omiljeniKorisnikServis.findById(id), HttpStatus.OK);
     }
 
     @ApiOperation(value = "Obrisi Omiljenog Korisnika po id")
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> obrisiOmiljenogKorisnika(@RequestHeader("Authorization") String authorization, @PathVariable("id") Long id){
+    public ResponseEntity<?> obrisiOmiljenogKorisnika(@PathVariable("id") Long id){
         omiljeniKorisnikServis.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
