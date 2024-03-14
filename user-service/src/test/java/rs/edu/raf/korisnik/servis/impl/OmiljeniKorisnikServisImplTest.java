b65d009cfdc94a70ecc42741a2f0442224ca0fee
Petar_Stamenic_RN_77_21 <92359907+PetarStamenic@users.noreply.github.com>
2024-03-14 18:16:46 +0100

    Update OmiljeniKorisnikServisImplTest.java
    disabled

--- user-service/src/test/java/rs/edu/raf/korisnik/servis/impl/OmiljeniKorisnikServisImplTest.java
+++ user-service/src/test/java/rs/edu/raf/korisnik/servis/impl/OmiljeniKorisnikServisImplTest.java
@@ -15,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Disabled
 class OmiljeniKorisnikServisImplTest {
 
     @Autowired
