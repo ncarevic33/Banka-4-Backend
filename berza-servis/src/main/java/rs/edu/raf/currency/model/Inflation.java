91108bd26ce06baecfe8f9b63cf1c6584defc7dd
JelenaMijatovic <jmijatovic11421rn@raf.rs>
2024-03-17 17:51:48 +0100

    Inflation model

---
+++ berza-servis/src/main/java/rs/edu/raf/currency/model/Inflation.java
@@ -0,0 +1,31 @@
+package rs.edu.raf.currency.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@IdClass(CurrencyId.class)
+public class Inflation {
+
+    @Id
+    @NotBlank
+    private String currency; //CurrencyCode
+
+    @Id
+    @NotBlank
+    private String year;
+
+    @Id
+    @NotBlank
+    private String inflationRate;
+}
