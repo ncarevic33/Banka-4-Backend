893f2156163d212220709f768d46c8b0ac2ca05d
JelenaMijatovic <jmijatovic11421rn@raf.rs>
2024-03-25 20:22:16 +0100

    Dodate metode u Currency i Inflation repositorije

--- berza-servis/src/main/java/rs/edu/raf/currency/repository/CurrencyRepository.java
+++ berza-servis/src/main/java/rs/edu/raf/currency/repository/CurrencyRepository.java
@@ -3,5 +3,9 @@ package rs.edu.raf.currency.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rs.edu.raf.currency.model.Currency;
 
+import java.util.List;
+
 public interface CurrencyRepository extends JpaRepository<Currency, String> {
+
+    List<Currency> findAllByPolity(String polity);
 }
--- berza-servis/src/main/java/rs/edu/raf/currency/repository/InflationRepository.java
+++ berza-servis/src/main/java/rs/edu/raf/currency/repository/InflationRepository.java
@@ -4,5 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import rs.edu.raf.currency.model.Inflation;
 import rs.edu.raf.currency.model.InflationId;
 
+import java.util.List;
+
 public interface InflationRepository extends JpaRepository<Inflation, InflationId> {
+    List<Inflation> findAllByCurrency(String currencyCode);
+
+    List<Inflation> findAllByCurrencyAndInflYear(String currencyCode, String year);
 }
