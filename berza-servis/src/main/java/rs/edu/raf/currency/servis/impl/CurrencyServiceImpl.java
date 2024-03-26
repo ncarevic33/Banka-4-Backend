46dd673b05ef5a7690da41193ba9875dd4d6fdf4
JelenaMijatovic <jmijatovic11421rn@raf.rs>
2024-03-26 22:48:17 +0100

    CurrencyService implementiran

---
+++ berza-servis/src/main/java/rs/edu/raf/currency/servis/impl/CurrencyServiceImpl.java
@@ -0,0 +1,92 @@
+package rs.edu.raf.currency.servis.impl;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import rs.edu.raf.currency.dto.CurrencyDTO;
+import rs.edu.raf.currency.mapper.CurrencyMapper;
+import rs.edu.raf.currency.model.Currency;
+import rs.edu.raf.currency.repository.CurrencyRepository;
+import rs.edu.raf.currency.servis.CurrencyService;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CurrencyServiceImpl implements CurrencyService {
+
+    CurrencyRepository currencyRepository;
+    CurrencyMapper currencyMapper;
+
+    @Autowired
+    public CurrencyServiceImpl(CurrencyRepository currencyRepository, CurrencyMapper currencyMapper) {
+        this.currencyRepository = currencyRepository;
+        this.currencyMapper = currencyMapper;
+    }
+
+    @Override
+    public CurrencyDTO getCurrencyByCurrencyCode(String currencyCode) {
+        Currency c = currencyRepository.findById(currencyCode).orElse(null);
+        CurrencyDTO dto = currencyMapper.currencyToCurrencyDTO(c);
+        return dto;
+    }
+
+    @Override
+    public List<CurrencyDTO> getAllCurrencies() {
+        List<Currency> currencies = currencyRepository.findAll();
+        List<CurrencyDTO> dtos = new ArrayList<>();
+        for (Currency c : currencies) {
+            CurrencyDTO dto = currencyMapper.currencyToCurrencyDTO(c);
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+
+    @Override
+    public List<CurrencyDTO> getCurrenciesInPolity(String polity) {
+        List<Currency> currencies = currencyRepository.findAllByPolity(polity);
+        List<CurrencyDTO> dtos = new ArrayList<>();
+        for (Currency c : currencies) {
+            CurrencyDTO dto = currencyMapper.currencyToCurrencyDTO(c);
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+
+    @PostConstruct
+    @Scheduled(cron = "0 0 10 * * *")
+    public void updateCurrencies() {
+        List<Currency> currencies = new ArrayList<>();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://www.alphavantage.co/physical_currency_list/")).build();
+        HttpResponse<String> response = null;         // FVHPQRIRKBYHSTJU
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String body = response.body();
+        String[] lines = body.substring(body.indexOf("\n")+1).split("\n");
+        for (String l : lines) {
+            String[] c = l.split(",");
+            Currency currency = new Currency();
+            currency.setCurrencyCode(c[0]);
+            currency.setCurrencyName(c[1]);
+            try {
+                //System.out.println(java.util.Currency.getInstance(c[0]).getSymbol());
+                currency.setCurrencySymbol(java.util.Currency.getInstance(c[0]).getSymbol());
+            } catch (NullPointerException | IllegalArgumentException e) {
+                currency.setCurrencySymbol(c[0]);
+            }
+            currency.setPolity("placeholder"); //TODO find api
+            currencies.add(currency);
+        }
+        currencyRepository.saveAll(currencies);
+    }
+}
