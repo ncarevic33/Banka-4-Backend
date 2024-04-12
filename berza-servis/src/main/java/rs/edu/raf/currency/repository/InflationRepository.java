994ee0ae1c4e8d5c189b99be74975b4ebf1afc0a
Petar_Stamenic_RN_77_21 <92359907+PetarStamenic@users.noreply.github.com>
2024-04-12 18:38:06 +0200

    Update InflationRepository.java

--- berza-servis/src/main/java/rs/edu/raf/currency/repository/InflationRepository.java
+++ berza-servis/src/main/java/rs/edu/raf/currency/repository/InflationRepository.java
@@ -1,11 +1,13 @@
 package rs.edu.raf.currency.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import rs.edu.raf.currency.model.Inflation;
 import rs.edu.raf.currency.model.InflationId;
 
 import java.util.List;
 
+@Repository
 public interface InflationRepository extends JpaRepository<Inflation, InflationId> {
     List<Inflation> findAllByCountry(String country);
 
