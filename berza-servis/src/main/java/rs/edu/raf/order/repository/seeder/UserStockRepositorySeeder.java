0c5b69bac87887c2a0047d211f5e62492176dab4
Marko Mihailovic <89168096+mmihailovic@users.noreply.github.com>
2024-06-11 15:08:11 +0200

    Seeder fix (#201)
    * otc fix
    
    * otc fix
    
    * otc fix
    
    * otc fix
    
    * seeder fix
    
    * seeder fix
    
    * seeder fix
    
    * seeder fix
    
    * futures fix
    
    * futures fix

--- berza-servis/src/main/java/rs/edu/raf/order/repository/seeder/UserStockRepositorySeeder.java
+++ berza-servis/src/main/java/rs/edu/raf/order/repository/seeder/UserStockRepositorySeeder.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import rs.edu.raf.order.model.UserStock;
+import rs.edu.raf.order.repository.PonudaRepository;
 import rs.edu.raf.order.repository.UserStockRepository;
 
 import java.math.BigDecimal;
@@ -13,42 +14,48 @@ public class UserStockRepositorySeeder implements CommandLineRunner {
 
     @Autowired
     private UserStockRepository userStockRepository;
+    @Autowired
+    private PonudaRepository ponudaRepository;
 
     @Override
     public void run(String... args) throws Exception {
         // User stockovi za nasu banku Banka4
+        ponudaRepository.deleteAll();
+
         userStockRepository.deleteAll();
-//        UserStock newUserStock = new UserStock();
-//        newUserStock.setUserId(-1L);
-//        newUserStock.setTicker("AAPL");
-//        newUserStock.setQuantity(100);
-//        newUserStock.setCurrentAsk(new BigDecimal(10));
-//        newUserStock.setCurrentBid(new BigDecimal(10));
-//
-//        UserStock newUserStock1 = new UserStock();
-//        newUserStock1.setUserId(-1L);
-//        newUserStock1.setTicker("SONY");
-//        newUserStock1.setQuantity(100);
-//        newUserStock1.setCurrentAsk(new BigDecimal(10));
-//        newUserStock1.setCurrentBid(new BigDecimal(10));
-//
-//        UserStock newUserStock2 = new UserStock();
-//        newUserStock2.setUserId(-1L);
-//        newUserStock2.setTicker("NFLX");
-//        newUserStock2.setQuantity(100);
-//        newUserStock2.setCurrentAsk(new BigDecimal(10));
-//        newUserStock2.setCurrentBid(new BigDecimal(10));
-//
-//        UserStock newUserStock3 = new UserStock();
-//        newUserStock3.setUserId(-1L);
-//        newUserStock3.setTicker("SBUX");
-//        newUserStock3.setQuantity(100);
-//        newUserStock3.setCurrentAsk(new BigDecimal(10));
-//        newUserStock3.setCurrentBid(new BigDecimal(10));
-//
-//        userStockRepository.save(newUserStock);
-//        userStockRepository.save(newUserStock1);
-//        userStockRepository.save(newUserStock2);
-//        userStockRepository.save(newUserStock3);
+        if(userStockRepository.findAll().size() == 0) {
+            UserStock newUserStock = new UserStock();
+            newUserStock.setUserId(-1L);
+            newUserStock.setTicker("AAPL");
+            newUserStock.setQuantity(100);
+            newUserStock.setCurrentAsk(new BigDecimal(10));
+            newUserStock.setCurrentBid(new BigDecimal(10));
+
+            UserStock newUserStock1 = new UserStock();
+            newUserStock1.setUserId(-1L);
+            newUserStock1.setTicker("SONY");
+            newUserStock1.setQuantity(100);
+            newUserStock1.setCurrentAsk(new BigDecimal(10));
+            newUserStock1.setCurrentBid(new BigDecimal(10));
+
+            UserStock newUserStock2 = new UserStock();
+            newUserStock2.setUserId(-1L);
+            newUserStock2.setTicker("NFLX");
+            newUserStock2.setQuantity(100);
+            newUserStock2.setCurrentAsk(new BigDecimal(10));
+            newUserStock2.setCurrentBid(new BigDecimal(10));
+
+            UserStock newUserStock3 = new UserStock();
+            newUserStock3.setUserId(-1L);
+            newUserStock3.setTicker("SBUX");
+            newUserStock3.setQuantity(100);
+            newUserStock3.setCurrentAsk(new BigDecimal(10));
+            newUserStock3.setCurrentBid(new BigDecimal(10));
+
+            userStockRepository.save(newUserStock);
+            userStockRepository.save(newUserStock1);
+            userStockRepository.save(newUserStock2);
+            userStockRepository.save(newUserStock3);
+        }
     }
 }
